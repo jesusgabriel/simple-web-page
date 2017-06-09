bf93b60d598210a42814490735d944b13bb24ae7
jesusgabriel <amelo@fvi.edu>
2017-06-08 22:04:02 -0400

    save

--- app/components/Header.js
+++ app/components/Header.js
@@ -3,7 +3,16 @@ import React from 'react';
 function Header(props) {
   return (
     <div className='header'>
-      <p>I am a {props.title}!!</p>
+      {/* <p>I am a {props.title}!!</p> */}
+      <h1>Project Final</h1>
+      <h2>Andy Melo</h2>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
     </div>
   );
 }
